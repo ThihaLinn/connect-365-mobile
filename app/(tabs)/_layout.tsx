6780ea70a86b07bd9723d5a5d69de00aec28cacc
Thiha Linn <linthiha964@gmail.com>
2025-02-17 13:09:45 +0630

    remove dark mode

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -20,7 +20,7 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: 'blue',
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
